9d3b7503421a7beed5ed4f1f0f081c0b09ccea15
Onoja Victor <onoja.jsdev@gmail.com>
2023-12-02 01:08:15 +0100

    install missing depency- react-icons

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "51fa2bbafd8e845a"; }
+/******/ 		__webpack_require__.h = function() { return "a1ab23959312e0b3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
