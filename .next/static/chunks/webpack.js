7796f3a7158919ad00f121bd72fb6caa1fe9dd36
Onoja Victor <onoja.jsdev@gmail.com>
2024-01-05 14:19:22 +0100

    webpack compilation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dff43d90f22e38c8"; }
+/******/ 		__webpack_require__.h = function() { return "8de2097a55c673aa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
